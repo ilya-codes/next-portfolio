7ac2872c43871c93481b6437a0fc006141d86c9d
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-04-16 14:53:30 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,27 @@ import Layout from "../components/Layout";
 import "../styles/global.scss";
 
 import Spinner from "../components/Spinner";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
+  // const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const handleStart = (url) => {
-      url !== router.pathname ? setLoading(true) : setLoading(false);
-    };
-    const handleComplete = (url) => setLoading(false);
+    setLoading(false);
+  }, []);
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-  }, [router, loading]);
+  // useEffect(() => {
+  //   const handleStart = (url) => {
+  //     url !== router.pathname ? setLoading(true) : setLoading(false);
+  //   };
+  //   const handleComplete = (url) => setLoading(false);
+
+  //   router.events.on("routeChangeStart", handleStart);
+  //   router.events.on("routeChangeComplete", handleComplete);
+  //   router.events.on("routeChangeError", handleComplete);
+  // }, [router, loading]);
 
   return (
     <>
