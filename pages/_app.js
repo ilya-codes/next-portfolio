20d321fc01b70b6fbfe2aa14129d03c28d442b7a
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-04-16 14:05:35 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,29 @@ import Layout from "../components/Layout";
 import "../styles/global.scss";
 import Spinner from "../components/Spinner";
 
+import Router from "next/router";
 import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [ready, setReady] = useState(false);
+  const [ready, setReady] = useState(true);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   setReady(true);
+  // }, []);
+
+  Router.onRouteChangeStart = (url) => {
+    // Some page has started loading
+    setReady(false);
+  };
+
+  Router.onRouteChangeComplete = (url) => {
+    // Some page has finished loading
     setReady(true);
-  }, []);
+  };
+
+  Router.onRouteChangeError = (err, url) => {
+    // an error occurred.
+  };
 
   return (
     <>
