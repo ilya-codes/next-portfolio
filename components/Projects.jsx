4c6e2cf519a93b356f14375ea29d5225553e511f
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-05-03 16:08:02 +0900

    local db added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,36 +1,29 @@
 import projectsStyles from "../styles/Projects.module.scss";
 import Project from "./Project";
 import Details from "./Details";
-import image1 from "../public/tvshow-guide.png";
-import image2 from "../public/recipes.png";
+import { data } from "../data";
 
 const Projects = () => {
+  let isRight = false;
+
   return (
     <div className={projectsStyles.projects}>
-      <Project
-        img={image1}
-        prev="https://ilya-codes.github.io/react-tv-show-guide"
-        github="https://github.com/ilya-codes/react-tv-show-guide/tree/master"
-        details={
-          <Details
-            title="TV Show guide App"
-            text="Build with React, React-Context, third party API"
-          />
-        }
-      />
-      <Project
-        img={image2}
-        prev="https://ilya-codes.github.io/react-recipes-app"
-        github="https://github.com/ilya-codes/react-recipes-app/tree/master"
-        details={
-          <Details
-            title="Recipes App"
-            text="Build with React, React-Context, third party API"
-            isRight
-          />
-        }
-        isRight
-      />
+      {data.map((project) => (
+        <Project
+          key={project.id}
+          img={project.image}
+          prev={project.preview}
+          github={project.github}
+          isRight={project.id % 2 ? isRight : !isRight}
+          details={
+            <Details
+              title={project.title}
+              text={project.description}
+              isRight={project.id % 2 ? isRight : !isRight}
+            />
+          }
+        />
+      ))}
     </div>
   );
 };
---
+++ data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    image: "/tvshow-guide.png",
+    preview: "https://ilya-codes.github.io/react-tv-show-guide",
+    github: "https://github.com/ilya-codes/react-tv-show-guide/tree/master",
+    title: "TV Show guide App",
+    description: "Build with React, React-Context, third party API",
+  },
+  {
+    id: 2,
+    image: "/recipes.png",
+    preview: "https://ilya-codes.github.io/react-recipes-app",
+    github: "https://github.com/ilya-codes/react-recipes-app/tree/master",
+    title: "Recipes App",
+    description: "Build with React, React-Context, third party API",
+  },
+];
