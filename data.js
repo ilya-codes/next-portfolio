1be11ee9eec205d1c470eaf9913a55846506f378
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-11-22 10:46:50 +0900

    new projects added

--- data.js
+++ data.js
@@ -1,21 +1,39 @@
+import mindboard from "./public/mindboard.png";
 import tvshowImage from "./public/tvshow-guide.png";
 import recipesImage from "./public/recipes.png";
+import qr from "./public/qr.png";
 
 export const data = [
   {
     id: 1,
+    image: mindboard,
+    preview: "https://mind-board-ilya-codes.vercel.app/",
+    github: "https://github.com/ilya-codes/mind-board",
+    title: "Mindboard",
+    description: "Build with Next.js, Tailwind and Firebase",
+  },
+  {
+    id: 2,
     image: tvshowImage,
     preview: "https://ilya-codes.github.io/react-tv-show-guide",
     github: "https://github.com/ilya-codes/react-tv-show-guide/tree/master",
     title: "TV Show guide App",
-    description: "Build with React, React-Context, third party API",
+    description: "Build with React, Styled Components and Tvmaze API",
   },
   {
-    id: 2,
+    id: 3,
     image: recipesImage,
     preview: "https://ilya-codes.github.io/react-recipes-app",
     github: "https://github.com/ilya-codes/react-recipes-app/tree/master",
     title: "Recipes App",
-    description: "Build with React, React-Context, third party API",
+    description: "Build with React, Styled Components and Edamam API",
+  },
+  {
+    id: 4,
+    image: qr,
+    preview: "https://ilya-codes.github.io/easy-qr-generator",
+    github: "https://github.com/ilya-codes/easy-qr-generator",
+    title: "Easy Qr Code Generator",
+    description: "Vanilla Javascript, Bootstrap styled",
   },
 ];
