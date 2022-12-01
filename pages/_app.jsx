38b783f25a14c9511aabc653595c420b52220c61
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-12-01 12:55:38 +0900

    analytics fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,16 @@ import ScrollToTop from "../components/ScrollToTop";
 import Layout from "../components/Layout";
 import Spinner from "../components/Spinner";
 import { useEffect, useState } from "react";
+import { analytics } from "../utils/firebase";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      analytics();
+    }
     setLoading(false);
   }, []);
 
