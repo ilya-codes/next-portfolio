0b6575a31b7e0144bb1c9bd7e5a214c9c17b0bb6
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-12-01 12:43:11 +0900

    analytics fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@ import ScrollToTop from "../components/ScrollToTop";
 import Layout from "../components/Layout";
 import Spinner from "../components/Spinner";
 import { useEffect, useState } from "react";
-import Script from "next/script";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
@@ -14,24 +13,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.TRACKING_CODE}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', ${process.env.TRACKING_CODE}, {
-page_path: window.location.pathname,
-});
-`,
-        }}
-      />
-
       {loading && <Spinner />}
       <ScrollToTop />
       <Layout>
